c586f650422d688c0d576461ff4eaff99fe4dbc5
Unknown <johacris3@gmail.com>
2018-12-04 21:42:21 -0500

    a thread can have replies

--- forum-course/app/Http/Controllers/ThreadsController.php
+++ forum-course/app/Http/Controllers/ThreadsController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use App\Thread;
 use Illuminate\Http\Request;
 
-class ThreadController extends Controller
+class ThreadsController extends Controller
 {
     /**
      * Display a listing of the resource.
@@ -16,7 +16,7 @@ class ThreadController extends Controller
     {
         $threads = Thread::latest()->get();
         return view('threads.index', compact('threads'));
-    }
+    } 
 
     /**
      * Show the form for creating a new resource.
@@ -47,7 +47,7 @@ class ThreadController extends Controller
      */
     public function show(Thread $thread)
     {
-        //
+        return view('threads.show', compact('thread'));  
     }
 
     /**
---
+++ forum-course/resources/views/threads/show.blade.php
@@ -0,0 +1,42 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{$thread->title}}</div>
+
+                <div class="card-body">
+                    {{$thread->body}}
+                    <hr>
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            @foreach($thread->replies as $reply)
+            <div class="card">
+                
+                
+                <div class="card-header">
+                    <a href="#">
+                        {{$reply->owner->name}}
+                    </a> 
+                    said {{$reply->created_at}}...
+                </div>
+
+                <div class="card-body">
+                    {{$reply->body}}
+                    <hr>
+
+                </div>
+            </div>
+            @endforeach
+        </div>
+    </div>
+</div>
+@endsection
