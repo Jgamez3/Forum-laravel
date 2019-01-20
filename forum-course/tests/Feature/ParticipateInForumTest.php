c4a75d483ff01ea87ff24c855d617e9d57e500f4
Johana Gamez <johacris3@gmail.com>
2019-01-20 12:35:39 -0500

    how to test validation errors

--- forum-course/tests/Feature/ParticipateInForumTest.php
+++ forum-course/tests/Feature/ParticipateInForumTest.php
@@ -38,4 +38,22 @@ class ParticipateInForumTest extends TestCase
         $this->get($thread->path())
             ->assertSee($reply->title);
     }
+
+
+    /** @test */
+
+    function a_reply_requires_a_body (){
+
+        $this->withExceptionHandling()->signIn(); 
+
+        $thread = create('App\Thread');
+
+        $reply = make('App\Reply', ['body' =>null]);
+
+        $this->post($thread->path() . '/replies', $reply->toArray())
+        ->assertSessionHasErrors('body');
+
+        
+
+    }
 }
